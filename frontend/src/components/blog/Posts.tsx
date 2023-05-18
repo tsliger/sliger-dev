af1355995a48130f98c5b16f5696d9a965ec265c
tsliger <tsliger@protonmail.com>
2023-05-18 15:51:39 -0400

    added pagination to blog post page

--- frontend/src/components/blog/Posts.tsx
+++ frontend/src/components/blog/Posts.tsx
@@ -3,6 +3,7 @@ import ky from "ky"
 import PostCard from './PostCard';
 import Loading from '../utils/Loading';
 import NetworkIssue from './NetworkIssue';
+import ReactPaginate from 'react-paginate';
 
 export interface Post {
   title: string;
@@ -19,14 +20,24 @@ export default function Posts() {
   const [posts, setPosts] = useState([] as Post[]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [metadata, setMeta] = useState(null)
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const handlePageClick = async (event) => {
+    const newOffset = (event.selected * metadata.pagination.pageSize) % metadata.pagination.total;
+   
+    setItemOffset(newOffset);
+  };
 
   const getPosts = async () => {
+    setLoading(true)
     try {
-      const res: any = await ky.get("http://localhost:1337/api/posts?populate=*").json()
+      const res: any = await ky.get(`http://localhost:1337/api/posts?populate=*&pagination[page]=${itemOffset + 1}`).json()
       setPosts(res.data as []);
-      setInterval(() => {
+      setTimeout(() => {
         setLoading(false);
       }, 150)
+      setMeta(res.meta)
     } catch (error) {
       if (error.name === 'HTTPError') {
         const errorJson = await error.response.json();
@@ -40,7 +51,7 @@ export default function Posts() {
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [itemOffset]);
 
   if (loading) {
     return <Loading />;
@@ -49,7 +60,6 @@ export default function Posts() {
   if (error) {
     return <NetworkIssue />;
   }
-
 
   return (
     <div>
@@ -63,6 +73,23 @@ export default function Posts() {
 
           return <PostCard key={post.slug} post={post} authors={authors} categories={categories} image={image} postId={id}/>;
         })}
+      </div>
+      <div className="w-full grid place-items-center absolute -bottom-40">
+        {metadata.pagination && 
+          <ReactPaginate
+            className="flex space-x-10 items-center"
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={handlePageClick}
+            pageClassName="w-10 aspect-square grid place-items-center rounded-full text-[#FFE6C7]  p-3 text-xs font-mono border-2 border-black/20 font-bold shadow-lg active:shadow-none active:scale-95 duration-150 transition"
+            previousClassName="w-10 aspect-square grid place-items-center rounded-full text-[#FFE6C7]  p-3 text-xs font-mono border-2 border-black/20 font-bold shadow-lg active:shadow-none active:scale-95 duration-150 transition"
+            nextClassName="w-10 aspect-square grid place-items-center rounded-full text-[#FFE6C7]  p-3 text-xs font-mono border-2 border-black/20 font-bold shadow-lg active:shadow-none active:scale-95 duration-150 transition"
+            pageRangeDisplayed={5}
+            pageCount={metadata.pagination.pageCount}
+            previousLabel="<"
+            renderOnZeroPageCount={null}
+          />
+        }
       </div>
     </div>
   )
